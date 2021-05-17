63b23ce5a410dbbe959dfc1e9d9dd00e4d4b203d
Terry <choir0118@gmail.com>
2021-05-17 21:04:39 +0900

    feat: 35.Search Insert Position 답안 추가.

--- src/main/java/thkim/array/Main.java
+++ src/main/java/thkim/array/Main.java
@@ -1,4 +1,16 @@
 package thkim.array;
 
 public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    // https://leetcode.com/problems/search-insert-position/
+    public int searchInsert(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= target) return i;
+        }
+
+        return nums.length;
+    }
 }
