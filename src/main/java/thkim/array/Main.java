0ee6b6779023fbec08a652b32af9fb689c2419bd
Terry <choir0118@gmail.com>
2021-05-19 23:40:20 +0900

    feat: 53. Maximum Subarray 답안 추가.

--- src/main/java/thkim/array/Main.java
+++ src/main/java/thkim/array/Main.java
@@ -2,15 +2,37 @@ package thkim.array;
 
 public class Main {
     public static void main(String[] args) {
+        Solution solution = new Solution();
 
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(solution.maxSubArray(nums));
     }
 
-    // https://leetcode.com/problems/search-insert-position/
-    public int searchInsert(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] >= target) return i;
+    static class Solution {
+        // https://leetcode.com/problems/search-insert-position/
+        public int searchInsert(int[] nums, int target) {
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] >= target) return i;
+            }
+
+            return nums.length;
         }
 
-        return nums.length;
+        // https://leetcode.com/problems/maximum-subarray/
+        public int maxSubArray(int[] nums) {
+            int tempMax = nums[0];
+            int finalMax = tempMax;
+
+            for (int i = 1; i < nums.length; i++) {
+                tempMax = Math.max(nums[i], tempMax + nums[i]);
+
+                if (tempMax > finalMax) {
+                    finalMax = tempMax;
+                }
+            }
+
+            return finalMax;
+        }
     }
+
 }
