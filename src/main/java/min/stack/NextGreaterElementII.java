2492868bf4883baefbda4fcc1a7bb5b28a9962a4
kimmin <kimmin2301@gmail.com>
2021-06-04 00:00:45 +0900

    feat: Next Greater Element II 풀이 완료

---
+++ src/main/java/min/stack/NextGreaterElementII.java
@@ -0,0 +1,33 @@
+package min.stack;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class NextGreaterElementII {
+
+    public static void main(String[] args) {
+        System.out.println(1 % 3);
+
+        int[] result = nextGreaterElements(new int[]{1, 4, 3, 2, 5, 1});
+        System.out.println("result = " + Arrays.toString(result));
+    }
+
+    public static int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        Arrays.fill(res, -1);
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < n * 2; i++) {
+
+            while (!st.isEmpty() && nums[st.peek()] < nums[i % n]) {
+                res[st.pop()] = nums[i % n];
+            }
+
+            if (i < n) {
+                st.push(i);
+            }
+        }
+
+        return res;
+    }
+}
---
+++ src/main/java/min/stack/NextGreaterNodeInLinkedList.java
@@ -0,0 +1,40 @@
+package min.stack;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class NextGreaterNodeInLinkedList {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] nextLargerNodes(ListNode head) {
+
+        return null;
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
