9a9dd157d2e6f1fc9aee0451816058e8865e5f6a
kimmin <kimmin2301@gmail.com>
2021-06-05 21:18:22 +0900

    feat: Next Greater Node In Linked List 풀이 완료

--- src/main/java/min/stack/NextGreaterNodeInLinkedList.java
+++ src/main/java/min/stack/NextGreaterNodeInLinkedList.java
@@ -1,32 +1,34 @@
 package min.stack;
 
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Stack;
+
 public class NextGreaterNodeInLinkedList {
 
-    public static void main(String[] args) {
-
-    }
-
     public static int[] nextLargerNodes(ListNode head) {
+        ArrayList<Integer> input = new ArrayList<>();
+        while (Objects.nonNull(head)) {
+            input.add(head.val);
+            head = head.next;
+        }
 
-        return null;
+        int[] inputs = input.stream().mapToInt(v -> v).toArray();
+        int[] outputs = new int[inputs.length];
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < inputs.length; i++) {
+            while (!st.isEmpty() && inputs[st.peek()] < inputs[i]) {
+                outputs[st.pop()] = inputs[i];
+            }
+            st.push(i);
+        }
+
+        return outputs;
     }
 
     static class ListNode {
         int val;
         ListNode next;
-
-        ListNode() {
-        }
 
         ListNode(int val) {
             this.val = val;
---
+++ src/test/java/min/stack/NextGreaterNodeInLinkedListTest.java
@@ -0,0 +1,48 @@
+package min.stack;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static min.stack.NextGreaterNodeInLinkedList.ListNode;
+import static min.stack.NextGreaterNodeInLinkedList.nextLargerNodes;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class NextGreaterNodeInLinkedListTest {
+
+    @Test
+    void test_1() {
+        // given
+        ListNode node3 = new ListNode(5);
+        ListNode node2 = new ListNode(1, node3);
+        ListNode head = new ListNode(2, node2);
+
+        // when
+        int[] res = nextLargerNodes(head);
+
+        System.out.println("res = " + Arrays.toString(res));
+
+        // then
+        assertTrue(Arrays.equals(new int[]{5, 5, 0}, res));
+    }
+
+    @Test
+    void test_2() {
+        // given
+        ListNode node5 = new ListNode(5);
+        ListNode node4 = new ListNode(3, node5);
+        ListNode node3 = new ListNode(4, node4);
+        ListNode node2 = new ListNode(7, node3);
+        ListNode head = new ListNode(2, node2);
+
+        // when
+        int[] res = nextLargerNodes(head);
+
+        System.out.println("res = " + Arrays.toString(res));
+
+        // then
+        assertTrue(Arrays.equals(new int[]{7, 0, 5, 5, 0}, res));
+    }
+
+}
