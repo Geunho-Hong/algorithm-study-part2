94a8a474dc4c3bdb9e52ab3b94feef272028b340
Terry <choir0118@gmail.com>
2021-06-26 14:55:51 +0900

    feat : LinkedList Solution

---
+++ src/main/java/thkim/linkedlist/PartitionList.kt
@@ -0,0 +1,29 @@
+package thkim.linkedlist
+
+class PartitionList {
+    fun partition(head: ListNode?, x: Int): ListNode? {
+        var head = head
+
+        val lessHead = ListNode(0)
+        var lessLast: ListNode? = lessHead
+        val greaterHead = ListNode(0)
+        var greaterLast: ListNode? = greaterHead
+
+        while (head != null) {
+            if (head.`val` < x) {
+                lessLast!!.next = head
+                lessLast = lessLast.next
+            } else {
+                greaterLast!!.next = head
+                greaterLast = greaterLast.next
+            }
+
+            head = head.next
+        }
+
+        greaterLast!!.next = null
+
+        lessLast!!.next = greaterHead.next
+        return lessHead.next
+    }
+}
