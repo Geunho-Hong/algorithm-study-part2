ee148bda51d342c4bf8fc1eaa3e7d1a2ea975768
kimmin <kimmin2301@gmail.com>
2021-06-26 10:29:15 +0900

    feat: Reorder List 풀이 완료

--- src/main/java/min/linkedlist/ReorderList.java
+++ src/main/java/min/linkedlist/ReorderList.java
@@ -1,7 +1,5 @@
 package min.linkedlist;
 
-import java.util.ArrayDeque;
-
 public class ReorderList {
 
     public static void main(String[] args) {
@@ -15,28 +13,51 @@ public class ReorderList {
     }
 
     public static void reorderList(ListNode head) {
-        ArrayDeque<ListNode> deque = new ArrayDeque<>();
-        ListNode tempHead = head;
-        while (tempHead != null) {
-            deque.push(tempHead);
-            tempHead = tempHead.next;
+        if (head == null || head.next == null) return;
+
+        // Reverse 할 기준 노드인 중간 노드 탐색
+        var slow = head;
+        var fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
 
-        ListNode currNode = deque.poll();
-        int remain = 1;
-        while (!deque.isEmpty()) {
-            if (remain % 2 == 0) {
-                currNode.next = deque.poll();
-            } else {
-                currNode.next = deque.pop();
-            }
-            currNode = currNode.next;
-            remain++;
+        var middle = slow.next;
+        slow.next = null;
 
-            if (deque.size() == 1) {
-                deque.peek().next = null;
-            }
+        // 중간 노드 기준으로 List Reverse
+        ListNode reverseHead = reverse(middle);
+
+        // List 와 Reverse List 결합
+        var p1 = head;
+        var p2 = reverseHead;
+        while (true) {
+            var temp1 = p1.next;
+            p1.next = p2;
+
+            if (temp1 == null) break;
+
+            var temp2 = p2.next;
+            p2.next = temp1;
+            p1 = temp1;
+            p2 = temp2;
         }
+    }
+
+    static ListNode reverse(ListNode node) {
+        if (node.next == null) {
+            return node;
+        }
+
+        ListNode head = reverse(node.next);
+
+        var temp = node.next;
+        node.next = null;
+        temp.next = node;
+
+        return head;
     }
 
     static class ListNode {
