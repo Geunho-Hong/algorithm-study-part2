47501b83fd5ccb6fd23a437b7ac57b350c1e90c0
kimmin <kimmin2301@gmail.com>
2021-06-30 02:07:36 +0900

    feat: Binary Tree Inorder Traversal

---
+++ src/main/java/min/leetcode/PascalTriangle.java
@@ -0,0 +1,19 @@
+package min.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalTriangle {
+
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> rows = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+
+        }
+
+        return result;
+    }
+
+}
---
+++ src/main/java/min/tree/BinaryTreeInorderTraversal.java
@@ -0,0 +1,21 @@
+package min.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreeInorderTraversal {
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        inorder(root, res);
+        return res;
+    }
+
+    public void inorder(TreeNode node, List<Integer> res) {
+        if (node != null) {
+            inorder(node.left, res);
+            res.add(node.val);
+            inorder(node.right, res);
+        }
+    }
+}
