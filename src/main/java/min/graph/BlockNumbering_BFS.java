244b9f67fb568910f07ca58d9c4fb41990cbfa77
kimmin <kimmin2301@gmail.com>
2021-07-06 00:51:15 +0900

    feat: Add solution Freeze Drinks DFS

---
+++ src/main/java/min/graph/BlockNumbering_BFS.java
@@ -0,0 +1,11 @@
+package min.graph;
+
+public class BlockNumbering_BFS {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
---
+++ src/main/java/min/graph/BlockNumbering_DFS.java
@@ -0,0 +1,15 @@
+package min.graph;
+
+import java.util.Scanner;
+
+public class BlockNumbering_DFS {
+
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
