bb6fd091e20338a83aa44f12c6c7827fb874e0d2
kimmin <kimmin2301@gmail.com>
2021-07-24 00:19:35 +0900

    refactor: RedGreenColorWeakness bfs -> dfs

--- src/main/java/min/graph/dfs/RedGreenColorWeakness.java
+++ src/main/java/min/graph/dfs/RedGreenColorWeakness.java
@@ -1,4 +1,4 @@
-package min.graph.bfs;
+package min.graph.dfs;
 
 import java.io.BufferedReader;
 import java.io.IOException;
---
+++ src/main/java/min/graph/dfs/SheepRescueOperation.java
@@ -0,0 +1,13 @@
+package min.graph.dfs;
+
+public class SheepRescueOperation {
+
+    public static void main(String[] args) {
+
+    }
+
+    static void initMap() {
+        
+    }
+
+}
