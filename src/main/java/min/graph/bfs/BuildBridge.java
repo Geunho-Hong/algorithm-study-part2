9f7fcfd40f5291931315b727a88f7ea998c1b5e6
kimmin <kimmin2301@gmail.com>
2021-07-25 08:37:31 +0900

    feat: Add solution Sheep Rescue Operation

--- src/main/java/min/graph/bfs/BuildBridge.java
+++ src/main/java/min/graph/bfs/BuildBridge.java
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Queue;
-import java.util.Scanner;
 
 public class BuildBridge {
 
--- src/main/java/min/graph/dfs/RedGreenColorWeakness.java
+++ src/main/java/min/graph/dfs/RedGreenColorWeakness.java
@@ -3,7 +3,6 @@ package min.graph.dfs;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class RedGreenColorWeakness {
 
@@ -55,31 +54,14 @@ public class RedGreenColorWeakness {
         }
     }
 
-    static void weakness(int x, int y) {
-        normalVisited[x][y] = true;
-
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-        }
-    }
-
-    static void initVisited() {
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(normalVisited[i], false);
-        }
-    }
-
     static void initMap() throws IOException {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
             n = Integer.parseInt(br.readLine());
 
             normalDrawing = new char[n][n];
-            weaknessDrawing = new char[n][n];
-
             normalVisited = new boolean[n][n];
+
+            weaknessDrawing = new char[n][n];
             weaknessVisited = new boolean[n][n];
 
             for (int i = 0; i < n; i++) {
