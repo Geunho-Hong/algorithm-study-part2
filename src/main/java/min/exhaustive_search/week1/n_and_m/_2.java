c5e11a83f73937a2d97c24390702bbcea5850144
kimmin <kimmin2301@gmail.com>
2021-07-27 15:04:53 +0900

    feat: Add solution N과 M (2)

--- src/main/java/min/exhaustive_search/week1/n_and_m/_2.java
+++ src/main/java/min/exhaustive_search/week1/n_and_m/_2.java
@@ -1,10 +1,10 @@
-package min.exhaustive_search.week1;
+package min.exhaustive_search.week1.n_and_m;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class N과M_1 {
+public class _2 {
 
     static int n;
     static int m;
@@ -21,8 +21,7 @@ public class N과M_1 {
 
     static void dfs(int depth) {
         if (depth == m) {
-            for (int i : arr)
-                sb.append(i).append(" ");
+            for (int i : arr) sb.append(i).append(" ");
             System.out.println(sb.toString());
             sb.delete(0, sb.length());
             return;
@@ -30,6 +29,9 @@ public class N과M_1 {
 
         for (int i = 0; i < n; i++) {
             if (!visit[i]) {
+                // 완성된 수열은 오름차순이어야 하기 때문에 다음에 나올 값이 이전 값보다 작으면 가지치기
+                if (depth > 0 && arr[depth - 1] > i + 1) continue;
+
                 visit[i] = true;
                 arr[depth] = i + 1;
                 dfs(depth + 1);
