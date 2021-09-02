da696d1f7d40ea6ba525a895c281fa73a43f608a
kimmin <kimmin2301@gmail.com>
2021-09-02 12:21:29 +0900

    feat: Add solution 이친수

--- src/main/java/min/dp/week1/동전1.java
+++ src/main/java/min/dp/week1/동전1.java
@@ -1,39 +1,40 @@
-package min.dp;
+package min.dp.week1;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
-public class 동전2 {
+public class 동전1 {
 
     static int n, k;
 
     static int[] coin = new int[100];
     static int[] dp = new int[10001];
 
+    // https://www.youtube.com/watch?v=2IkdAk1Loek
     public static void main(String[] args) throws IOException {
         input();
 
         /*
-         *          1  2  3  4  5  6  7  8  9  10  11  12  13  14  15
-         * [0] 1    1  2  3  4  5  6  7  8  9  10  11  12  13  14  15
-         * [1] 5    0  0  0  0  1  2  3  4  5   2   3   4   5   6   3
-         * [2] 12   0  0  0  0  0  0  0  0  0   0   0   1   2   3   4
+         *    1  2  3  4  5  6  7  8  9  10
+         * 1  1  1  1  1  1  1  1  1  1   1
+         * 2  0  1  1  2  2  3  3  4  4   5
+         * 5  0  0  0  0  1  1  2  2  3   4
+         * dp 1  2  2  3  4  4  5  7  8  10
          *
-         * dp[j] = Math.min(dp[j], dp[j - coin[i] + 1);
+         *
+         * dp[k] += dp[k - coin[i]]; -> 이전 동전 가치의 경우의 수에서 현재 동전 가치의 경우의수를 합산해줘야함 그래서 += (ex. k = 10, 1+5+4)
          */
 
-        Arrays.fill(dp, 100001);
 
-        dp[0] = 0;
+        dp[0] = 1; // 같은 숫자에 대해서 뺄셈했을 때 무조건 경우의 수가 1이어야함
         for (int i = 0; i < n; i++) {
             for (int j = coin[i]; j <= k; j++) {
-                dp[j] = Math.min(dp[j], dp[j - coin[i]] + 1); // 최소 경우의수 계속 갱신
+                dp[j] += dp[j - coin[i]];
             }
         }
 
-        System.out.println(dp[k] == 100001 ? -1 : dp[k]);
+        System.out.println(dp[k]);
     }
 
     public static void input() throws IOException {
