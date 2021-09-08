e67a7705d75f737f414796d7cc826d3ebaf09a4c
kimmin <kimmin2301@gmail.com>
2021-09-08 20:21:41 +0900

    feat: Add LCS algorithm

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// Blog: https://velog.io/@emplam27/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EA%B7%B8%EB%A6%BC%EC%9C%BC%EB%A1%9C-%EC%95%8C%EC%95%84%EB%B3%B4%EB%8A%94-LCS-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-Longest-Common-Substring%EC%99%80-Longest-Common-Subsequence
+public class Main {
+
+    // LCS의 두 가지 의미
+    // 1. Longest Common Subsequence: 최장 공통 부분수열
+    // 2. Longest Common Substring: 최장 공통 부분문자열
+    private static int[][] LCS = new int[101][101];
+
+    public static void main(String[] args) {
+        String s1 = "ABCDEF"; // i (행)
+        String s2 = "GBCDFE"; // j (열)
+
+
+    }
+
+    static void init() throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+
+        }
+    }
+
+}
