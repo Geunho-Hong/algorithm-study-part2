f63ceb8561ae8128758c3c5258ad10873ecff67d
kimmin <kimmin2301@gmail.com>
2021-09-12 04:41:00 +0900

    refactor: 주석 추가

--- src/main/java/min/dp/week1/동전1.java
+++ src/main/java/min/dp/week1/동전1.java
@@ -8,8 +8,8 @@ public class 동전1 {
 
     static int n, k;
 
-    static int[] coin = new int[100];
-    static int[] dp = new int[10001];
+    static int[] coin; // 최대 100가지 종류 동전
+    static int[] dp; // 1 ~ 10,000
 
     // https://www.youtube.com/watch?v=2IkdAk1Loek
     public static void main(String[] args) throws IOException {
@@ -20,12 +20,12 @@ public class 동전1 {
          * 1  1  1  1  1  1  1  1  1  1   1
          * 2  0  1  1  2  2  3  3  4  4   5
          * 5  0  0  0  0  1  1  2  2  3   4
-         * dp 1  2  2  3  4  4  5  7  8  10
+         * dp 1  2  2  3  4  5  6  7  8  10
          *
          *
-         * dp[k] += dp[k - coin[i]]; -> 이전 동전 가치의 경우의 수에서 현재 동전 가치의 경우의수를 합산해줘야함 그래서 += (ex. k = 10, 1+5+4)
+         * dp[k] += dp[k - coin[i]];
+         * -> 이전 동전 가치의 경우의 수에서 현재 동전 가치의 경우의 수를 합산 해줘야함 그래서 += (ex. k = 10, 1 + 5 + 4)
          */
-
 
         dp[0] = 1; // 같은 숫자에 대해서 뺄셈했을 때 무조건 경우의 수가 1이어야함
         for (int i = 0; i < n; i++) {
@@ -42,6 +42,9 @@ public class 동전1 {
             String[] inputs = br.readLine().split(" ");
             n = Integer.parseInt(inputs[0]);
             k = Integer.parseInt(inputs[1]);
+
+            coin = new int[n];
+            dp = new int[k + 1];
 
             for (int i = 0; i < n; i++) {
                 coin[i] = Integer.parseInt(br.readLine());
