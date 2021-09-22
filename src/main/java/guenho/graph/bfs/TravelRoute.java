5071fbff4d8e83bcff26521f57fbae6306e5f286
GuenhoHong <hogu8159@naver.com>
2021-09-23 01:05:13 +0900

    feat : add TravelRoute

--- src/main/java/guenho/graph/bfs/TravelRoute.java
+++ src/main/java/guenho/graph/bfs/TravelRoute.java
@@ -1,44 +1,42 @@
 package guenho.graph.bfs;
 
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class TravelRoute {
 
     private static boolean[] visited;
+    private static ArrayList<String> answer;
 
     public static void main(String[] args) {
         String[][] tickets = {{"ICN","JFK"},{"HND","IAD"},{"JFK","HND"}};
         System.out.println(solution(tickets));
     }
 
-    private static String[] solution(String[][] tickets) {
-        String[] answer = {};
+    public static String[] solution(String[][] tickets) {
+        answer = new ArrayList<>();
+        visited = new boolean[tickets.length];
 
-        for (int i = 0; i < tickets.length; i++) {
-            for (int j = 0; j < tickets[i].length; j++) {
-                System.out.println(tickets[i][j] + " ");
-            }
-            System.out.println();
+        dfs("ICN","ICN",0,tickets);
+        Collections.sort(answer);
+
+        return answer.get(0).split(" ");
+    }
+
+
+    private static void dfs(String now, String result, int depth, String[][] tickets) {
+        // 사용 티켓 수가 같아진 경우
+        if(depth == tickets.length) {
+            answer.add(result);
+            return;
         }
 
-        return answer;
-    }
-
-    private static void bfs(int i) {
-        Queue<Node> queue = new ArrayDeque<>();
-
-
-
-    }
-
-    private static class Node {
-        int x;
-        int y;
-
-        public Node(int x, int y) {
-            this.x = x;
-            this.y = y;
+        for (int i = 0; i < tickets.length; i++) {
+            if(!visited[i] && tickets[i][0].equals(now)) {
+                visited[i] = true;
+                dfs(tickets[i][1],result + " " + tickets[i][1],depth+1,tickets);
+                visited[i] = false;
+            }
         }
     }
 
