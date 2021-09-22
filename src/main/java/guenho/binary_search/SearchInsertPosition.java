6ed3db5115fb1f67a773e10f3dba2efa647404f3
GuenhoHong <hogu8159@naver.com>
2021-09-22 23:17:00 +0900

    feat : add SearchInsert Position

---
+++ src/main/java/guenho/binary_search/SearchInsertPosition.java
@@ -0,0 +1,27 @@
+package guenho.binary_search;
+
+public class SearchInsertPosition {
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 5, 6};
+        int target = 7;
+        System.out.println(searchInsert(nums, target));
+    }
+
+    private static int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (target == nums[mid]) {
+                return mid;
+            } else if (target > nums[mid]) {
+                start = mid + 1;
+            } else if (target < nums[mid]) {
+                end = mid - 1;
+            }
+            // 이분 탐색이기 때문에 시작점을 땡겨온다
+        }
+        return start;
+    }
+}
