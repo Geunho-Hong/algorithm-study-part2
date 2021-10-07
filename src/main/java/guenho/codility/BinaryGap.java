83e9ba8d5124eb53b5a3649bba9e8e640025c2b7
GuenhoHong <hongu8159@gmail.com>
2021-10-07 16:12:34 +0900

    feat:Add passingThroughTruck

---
+++ src/main/java/guenho/codility/BinaryGap.java
@@ -0,0 +1,44 @@
+package guenho.codility;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class BinaryGap {
+    public static void main(String[] args) {
+        System.out.println(solution(1041));
+    }
+
+    private static String makeBinaryNum(int N) {
+        StringBuilder sb = new StringBuilder();
+
+
+        while (N > 0) {
+            sb.append(N % 2);
+            N /= 2;
+        }
+
+        return sb.reverse().toString();
+    }
+
+    private static int solution(int N) {
+
+        String binaryString = makeBinaryNum(1041);
+
+        int zeroCount = 0;
+        int gapLength = 0;
+
+        for (int i = 0; i < binaryString.length(); i++) {
+            if(binaryString.charAt(i) == '1') {
+                if(zeroCount > gapLength) {
+                    gapLength = zeroCount;
+                    zeroCount = 0;
+                }
+            } else {
+                zeroCount++;
+            }
+        }
+
+        return gapLength;
+    }
+}
---
+++ src/main/java/guenho/codility/CyclicRotation.java
@@ -0,0 +1,30 @@
+package guenho.codility;
+
+import java.util.Arrays;
+
+public class CyclicRotation {
+    public static void main(String[] args) {
+        //int[] arr = {3, 8, 9, 7, 6};
+        int[] arr = {1,2,3,4};
+
+        solution(arr, 4);
+    }
+
+    private static int[] solution(int[] arr, int K) {
+
+        if(arr.length == 0) {
+            return arr;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            int[] tempArr = arr.clone();
+            int lastNum = arr[arr.length -1];
+            for (int j = 0; j < arr.length - 1; j++) {
+                arr[j + 1] = tempArr[j];
+            }
+            arr[0] = lastNum;
+        }
+
+        return arr;
+    }
+}
